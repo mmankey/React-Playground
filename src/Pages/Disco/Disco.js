cd4d5651e98c920de53de50f0380d01f893953b2
Mark Mankey <mark.mankey@gmail.com>
2019-04-15 19:28:27 -0500

    disco start

---
+++ src/Pages/Disco/Disco.js
@@ -0,0 +1,17 @@
+// ./Pages/Disco.js
+
+class Disco extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        numBoxes: 10,
+        width: "100px",
+        height: "100px"
+    }
+  }
+  
+
+  render() {
+    return <div />;
+  }
+}
---
+++ src/Pages/Disco/SubComponents/DiscoBox.js
@@ -0,0 +1,5 @@
+// ./Pages/SubComponents/DiscoBox.js
+// This is a Stateless Functional Component
+
+
+const DiscoBox = props => <div style={props.style} />;
